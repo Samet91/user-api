645153b6089367ff8e6fa3b4c750f46d503ba514
Samet Uslu <samet_u@hotmail.de>
2021-10-27 09:13:50 +0200

    Add GET POST requests

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,36 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+const users = ['Julia', 'Paul', 'Anke', 'Samet'];
+
+app.post('/api/users', (request, response) => {
+  response.send(request.body.name);
+});
+
+app.delete('/api/users/:name', (request, response) => {
+  const indexOfName = users.findIndex(
+    (nameWanted) => nameWanted === request.params.name
+  );
+  if (indexOfName !== -1) {
+    users.splice(indexOfName, 1);
+    response.send(users);
+  } else {
+    response.status(404).send('not found');
+  }
+});
+
 app.get('/api/users/:name', (request, response) => {
-  response.send(request.params.name);
+  const isNameKnow = users.includes(request.params.name);
+  if (isNameKnow) {
+    response.send(request.params.name);
+  } else {
+    response.status(404).send("Sorry can't find that!");
+  }
 });
 
 app.get('/api/users', (_request, response) => {
-  const users = ['Lara', 'Lili', 'Lolo'];
   response.send(users);
 });
 
