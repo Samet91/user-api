bb0261da516bd059e13fadc327c1ab700978cd52
Samet Uslu <samet_u@hotmail.de>
2021-10-27 15:44:02 +0200

    Add post, login find methods

--- src/server.ts
+++ src/server.ts
@@ -2,36 +2,83 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
-
+//Middleware for parsing application/json
 app.use(express.json());
 
-const users = ['Julia', 'Paul', 'Anke', 'Samet'];
+const users = [
+  {
+    name: 'Iva',
+    username: 'iva94',
+    password: '123',
+  },
+  {
+    name: 'Walied',
+    username: 'walied91',
+    password: '12345',
+  },
+  {
+    name: 'Samet',
+    username: 'samet91',
+    password: '12346',
+  },
+  {
+    name: 'Muhamed',
+    username: 'muhamed95',
+    password: '12347',
+  },
+];
 
-app.post('/api/users', (request, response) => {
-  response.send(request.body.name);
+app.post('/api/login/', (request, response) => {
+  const findUser = request.body;
+  const existingUser =
+    users.find((user) => user.username === findUser.username) &&
+    users.find((user) => user.password === findUser.password);
+
+  if (existingUser) {
+    response.send('Logged in');
+  } else {
+    response.status(401).send('Password or username incorrect. Try again!');
+  }
 });
 
-app.delete('/api/users/:name', (request, response) => {
-  const indexOfName = users.findIndex(
-    (nameWanted) => nameWanted === request.params.name
+app.post('/api/users/', (request, response) => {
+  const newUser = request.body;
+  if (
+    typeof newUser.name !== 'string' ||
+    typeof newUser.username !== 'string' ||
+    typeof newUser.password !== 'string'
+  ) {
+    response.status(400).send('Missing properties');
+    return;
+  }
+  if (users.some((user) => user.username === newUser.username)) {
+    response.status(409).send('User already exists.');
+  } else {
+    users.push(newUser);
+    response.send(`${newUser.name} added.`);
+  }
+});
+
+app.delete('/api/users/:username', (request, response) => {
+  const deleteUser = users.findIndex(
+    (user) => user.username === request.params.username
   );
-  if (indexOfName !== -1) {
-    users.splice(indexOfName, 1);
-    response.send(users);
+  if (deleteUser !== -1) {
+    users.splice(deleteUser, 1);
+    response.send('Deleted');
   } else {
     response.status(404).send('not found');
   }
 });
 
-app.get('/api/users/:name', (request, response) => {
-  const isNameKnow = users.includes(request.params.name);
-  if (isNameKnow) {
-    response.send(request.params.name);
+app.get('/api/users/:username', (request, response) => {
+  const user = users.find((user) => user.username === request.params.username);
+  if (user) {
+    response.send(user);
   } else {
-    response.status(404).send("Sorry can't find that!");
+    response.status(404).send('This page is not here.');
   }
 });
-
 app.get('/api/users', (_request, response) => {
   response.send(users);
 });
