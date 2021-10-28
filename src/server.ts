6615d223cf86fa255c1373b6a6604a28e26dbc55
Samet Uslu <samet_u@hotmail.de>
2021-10-28 11:37:46 +0200

    Add .env file config this and added some codes of server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('No MONGODB_URI provided');
+}
+
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import { connectDatabase } from './utils/database';
 
 const app = express();
 const port = 3000;
@@ -107,6 +115,8 @@ app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+connectDatabase(process.env.MONGODB_URI).then(() =>
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  })
+);
